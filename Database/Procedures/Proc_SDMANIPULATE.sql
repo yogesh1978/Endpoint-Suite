6f7b604365a9614fadd9086e709905b090b0c726
yogesh1978 <yogesh1978@gmail.com>
2024-09-04 18:14:12 +0530

    Add files via upload

---
+++ Database/Procedures/Proc_SDMANIPULATE.sql
@@ -0,0 +1,96 @@
+--------------------------------------------------------
+--  File created - Wednesday-September-04-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure DELETE_HPPATCH_DEVICE
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "DELETE_HPPATCH_DEVICE" 
+ (
+ Var_devname IN NVARCHAR2
+ )
+ AS
+ BEGIN
+ DELETE FROM NVD_DESTATUS WHERE NVD_ZOBJDEV = Var_devname;
+ DELETE FROM NVD_BUSTATUS WHERE NVD_ZOBJDEV = Var_devname;
+ DELETE FROM NVD_RESTATUS WHERE NVD_ZOBJDEV = Var_devname;
+ DELETE FROM NVD_PASTATUS WHERE NVD_ZOBJDEV = Var_devname;
+ DELETE FROM NVD_DEERROR WHERE NVD_ZOBJDEV = Var_devname;
+ END DELETE_HPPATCH_DEVICE;
+--------------------------------------------------------
+--  DDL for Procedure SDMANIPULATE
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "SDMANIPULATE" (
+ SUBNETIP in VARCHAR2
+)
+AS
+begin
+
+ DBMS_OUTPUT.PUT_LINE('SubnetIp: ' || SUBNETIP);
+
+ DELETE from 
+ NETDEVICECONFIG 
+ where exists (
+ select 1 from NETDEVICETEMPCONFIG s, NETDEVICECONFIG target
+ where s.SLNO != target.SLNO
+ and SUBNETIP = target.SUBNETADDR 
+ AND target.macaddr = s.macaddr
+ );
+
+ MERGE INTO NETDEVICECONFIG TARGET
+ USING NETDEVICETEMPCONFIG SOURCE
+ ON (TARGET.SUBNETADDR = SUBNETIP AND target.macaddr = source.macaddr)
+
+ WHEN MATCHED THEN
+
+ UPDATE SET 
+ target.status = 'Found',
+ target.device_id = source.device_id,
+ target.mtime = source.mtime,
+ target.devicetype = source.devicetype,
+ target.ipaddr = source.ipaddr,
+ target.os = source.os,
+ target.os_level = source.os_level,
+ 
+ 
+ target.slno = source.slno,
+ target.domain = source.domain,
+ target.workgroup = source.workgroup,
+ target.model = source.model,
+ target.netbios = source.netbios,
+ target.dclass = source.dclass,
+ target.manufacturer = source.manufacturer
+ WHEN NOT MATCHED THEN
+ INSERT (status,device_id,mtime,devicetype,ipaddr,os,os_level,subnetaddr,macaddr,slno,domain,workgroup,model,netbios,dclass,manufacturer)
+ VALUES ('Found',source.device_id,source.mtime,source.devicetype,source.ipaddr,source.os,source.os_level,source.subnetaddr,source.macaddr,source.slno,source.domain,source.workgroup,source.model,source.netbios,source.dclass,source.manufacturer);
+
+ UPDATE NETDEVICECONFIG
+ SET status = 'Missing'
+ where not exists (select 1 from NETDEVICETEMPCONFIG 
+ where SUBNETIP = NETDEVICECONFIG.SUBNETADDR
+ AND NETDEVICETEMPCONFIG.macaddr = NETDEVICECONFIG.macaddr) AND SUBNETIP = NETDEVICECONFIG.SUBNETADDR; 
+
+ DELETE from 
+ NETDEVICECONFIG 
+ where exists (
+ select 1 from Deviceconfig where
+ NETDEVICECONFIG.IPADDR = DeviceConfig.ipaddr
+ and NETDEVICECONFIG.SUBNETADDR = DeviceConfig.subnetaddr
+ AND NETDEVICECONFIG.macaddr = DeviceConfig.macaddr
+ );
+
+
+
+
+ FOR data IN (SELECT * FROM NETDEVICETEMPCONFIG where subnetaddr = SUBNETIP)
+ LOOP
+ DBMS_OUTPUT.PUT_LINE('Column1: ' || data.IPADDR);
+ END LOOP;
+ 
+ 
+ DELETE FROM NETDEVICETEMPCONFIG where SUBNETADDR = SUBNETIP;
+ commit;
+end;
---
+++ Database/Procedures/SP_FLAGCURRENTCOMPUTERS.sql
@@ -0,0 +1,42 @@
+--------------------------------------------------------
+--  DDL for Procedure SP_FLAGCURRENTCOMPUTERS
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "SP_FLAGCURRENTCOMPUTERS" 
+(vDaysNonCurrent IN INTEGER, vDaysCurrent IN INTEGER)
+IS
+
+BEGIN UPDATE rcaWindowsComputerUsers
+SET CurrentComputer = 1
+WHERE WindowsComputerUser_id IN
+(SELECT tData.WindowsComputerUser_id
+FROM
+(SELECT U.WindowsComputerUser_id
+FROM rcaWindowsComputerUsers U, rcaWindowsComputers C
+WHERE (U.WindowsComputer_id = C.WindowsComputer_id) AND (C.LastCollection >= (SYSDATE - vDaysCurrent))) tData); UPDATE rcaWindowsComputerUsers
+SET CurrentComputer = 0
+WHERE WindowsComputerUser_id IN
+(SELECT tSet1.WindowsComputerUser_id
+FROM
+(SELECT U.WindowsComputerUser_id, C.ComputerName_id, C.LastCollection
+FROM rcaWindowsComputerUsers U, rcaWindowsComputers C, rcaComputerNames N
+WHERE (U.WindowsComputer_id = C.WindowsComputer_id) AND (C.ComputerName_id = N.ComputerName_id) AND N.ComputerName_id IN 
+(SELECT ComputerName_id FROM (SELECT ComputerName_id, COUNT(WindowsSid_id) x FROM rcaWindowsComputers Group by ComputerName_id) t2 WHERE t2.x > 1)
+) tSet1,
+(SELECT C.ComputerName_id, MAX(C.LastCollection) MaxCollection 
+FROM rcaWindowsComputerUsers U, rcaWindowsComputers C, rcaComputerNames N
+WHERE (U.WindowsComputer_id = C.WindowsComputer_id) AND (C.ComputerName_id = N.ComputerName_id) AND N.ComputerName_id IN 
+(SELECT ComputerName_id FROM (SELECT ComputerName_id, COUNT(WindowsSid_id) x FROM rcaWindowsComputers Group by ComputerName_id) t2 WHERE t2.x > 1)
+GROUP BY C.ComputerName_id) tSet2
+WHERE (tSet1.ComputerName_id = tSet2.ComputerName_id) AND (tSet1.LastCollection < tSet2.MaxCollection)); UPDATE rcaWindowsComputerUsers
+SET CurrentComputer = 0
+WHERE WindowsComputerUser_id IN
+(SELECT tData.WindowsComputerUser_id
+FROM
+(SELECT U.WindowsComputerUser_id
+FROM rcaWindowsComputerUsers U, rcaWindowsComputers C
+WHERE (U.WindowsComputer_id = C.WindowsComputer_id) AND (C.LastCollection < (SYSDATE - vDaysNonCurrent))) tData);
+
+
+END;
